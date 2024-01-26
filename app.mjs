023d781e93ea23c6044f930e4832cafb1991c8e6
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-01-26 00:28:52 -0500

    misc: added comment

--- app.mjs
+++ app.mjs
@@ -2,11 +2,13 @@ import express from "express";
 import url from "url";
 import path from "path";
 import pool from "./db.js";
-
+// Documentation on how to use "pool" for querying db: https://node-postgres.com/apis/pool#poolquery
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
+
+
 
 
 // TODO add middleware and route handlers here
