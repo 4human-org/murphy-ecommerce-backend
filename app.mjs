99496d488db73bc90df6df44db1edf02a5aff914
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-01-26 20:53:25 -0500

    feat: added prisma

--- app.mjs
+++ app.mjs
@@ -1,21 +1,28 @@
 import express from "express";
-import url from "url";
+import { fileURLToPath } from "url";
 import path from "path";
-import pool from "./db.js";
-// Documentation on how to use "pool" for querying db: https://node-postgres.com/apis/pool#poolquery
+import { PrismaClient } from '@prisma/client';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
-
-// TODO add middleware and route handlers here
-
+const prisma = new PrismaClient();
 
 app.use(express.static("public"));
 app.use(express.json());
+
+// TODO add more middleware and route handlers here
+
+app.get('/item', async (req, res) => {
+  try {
+    const item = await prisma.item.findMany();
+    res.json(item);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred while fetching items' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
