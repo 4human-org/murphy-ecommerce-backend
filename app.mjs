43c26d446c5a3d6a8e41abc7a98f779713b1dd97
Bryant To <bryantto08@gmail.com>
2024-01-26 21:51:25 -0500

    GET and LIST item API Endpoints implemented

--- app.mjs
+++ app.mjs
@@ -14,16 +14,45 @@ app.use(express.json());
 
 // TODO add more middleware and route handlers here
 
-app.get('/item', async (req, res) => {
+/*
+GET API endpoint for Listing Items in the Database based on query parameters
+TODO: We need to filter by query params
+    Types of Filters:
+        Name, Price, Category, Is_Recommended, Stock (show if not 0), expiration-date
+*/
+app.get('/items', async (req, res) => {
   try {
-    const item = await prisma.item.findMany();
+    // Retrieving all items from database
+    let items = await prisma.item.findMany();
+
     res.json(item);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'An error occurred while fetching items' });
+    res.status(500).json({ error: 'An error occurred while fetching items'});
   }
 });
+
+// GET API endpoint for retrieving a single item in the Database based on item-id
+app.get('/item/:id', async (req, res) => {
+    try {
+      const item = await prisma.item.findUnique({
+        where: {
+            id: req.params.id
+        }
+      });
+      // Returns 404 Error if Item is not found
+      if (!item) {
+        res.status(404).json({error: 'Item not found'});
+      }
+      res.json(item);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'An error occurred while fetching items' });
+    }
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
