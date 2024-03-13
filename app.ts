e90a8cac9d8d620df7153b47878d7783c099f526
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-03-13 15:05:55 -0400

    feat: added typescript

--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
 // app.mjs
 import express from "express";
 import bodyParser from "body-parser";
-import { db } from "./firebase.mjs"; // Make sure to update the file extension here as well
+import { db } from "./firebase"; // Make sure to update the file extension here as well
 
+// Create a new Express application
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(bodyParser.json());
 
+type item = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  categories: string[];
+  imageUrl: string;
+  stock: number;
+  sourcing: string;
+};
+
+// Define Route for fetching all items
 app.get("/items", async (req, res) => {
   const itemsRef = db.collection("items");
   const snapshot = await itemsRef.get();
@@ -17,24 +30,24 @@ app.get("/items", async (req, res) => {
     return res.status(404).send("No items found");
   }
 
-  const items = [];
+  const items: item[] = [];
   snapshot.forEach((doc) => {
-    items.push({ id: doc.id, ...doc.data() });
+    items.push(doc.data() as item);
   });
 
   res.status(200).json(items);
 });
 
-app.get('/item/:id', async (req, res) => {
+app.get("/item/:id", async (req, res) => {
   try {
     const itemId = req.params.id;
 
     // Retrieve the item from Firestore using the provided item ID
-    const itemRef = await db.collection('items').doc(itemId).get();
-    
+    const itemRef = await db.collection("items").doc(itemId).get();
+
     // Returns 404 Error if Item is not found
     if (!itemRef.exists) {
-      return res.status(404).json({ error: 'Item not found' });
+      return res.status(404).json({ error: "Item not found" });
     }
 
     // Extract the data from the document snapshot
@@ -42,13 +55,13 @@ app.get('/item/:id', async (req, res) => {
     res.json(item);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'An error occurred while fetching item' });
+    res.status(500).json({ error: "An error occurred while fetching item" });
   }
 });
 
-app.post('/items', async (req, res) => {
+app.post("/items", async (req, res) => {
   try {
-    const newItemRef = await db.collection('items').add(req.body);
+    const newItemRef = await db.collection("items").add(req.body);
     const newItemSnapshot = await newItemRef.get();
     if (!newItemSnapshot.exists) {
       return res.status(404).json({ error: "Failed to create item" });
@@ -57,17 +70,19 @@ app.post('/items', async (req, res) => {
     res.status(200).json(newItem);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "An error occurred while creating a new item" });
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating a new item" });
   }
 });
 
-app.delete('/items/:id', async (req, res) => {
+app.delete("/items/:id", async (req, res) => {
   try {
     const itemId = req.params.id;
 
     // Delete the item from Firestore using the provided item ID
-    await db.collection('items').doc(itemId).delete();
-    
+    await db.collection("items").doc(itemId).delete();
+
     res.status(200).json({ message: "Item deleted successfully" });
   } catch (error) {
     console.log(error);
@@ -75,21 +90,21 @@ app.delete('/items/:id', async (req, res) => {
   }
 });
 
-app.patch('/items', async (req, res) => {
+app.patch("/items", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const itemId = req.body.id;
     console.log("itemID: " + itemId);
     if (!itemId) {
-      return res.status(400).json({ error: 'Item ID not provided' });
+      return res.status(400).json({ error: "Item ID not provided" });
     }
 
     // Retrieve the item from Firestore using the provided item ID
-    const itemRef = db.collection('items').doc(itemId);
+    const itemRef = db.collection("items").doc(itemId);
     const itemSnapshot = await itemRef.get();
 
     if (!itemSnapshot.exists) {
-      return res.status(404).json({ error: 'Item not found' });
+      return res.status(404).json({ error: "Item not found" });
     }
 
     // Extract updated fields from the request body
@@ -106,31 +121,9 @@ app.patch('/items', async (req, res) => {
     res.status(200).json(updatedItem);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'An error occurred while updating the item' });
-  }
-});
-
-
-app.get('/items-name', async (req, res) => {
-  try {
-    // Retrieving all items from the "items" collection
-    const snapshot = await db.collection('items').get();
-
-    if (snapshot.empty) {
-      return res.status(404).json({ error: 'No items found' });
-    }
-
-    // Extracting only item names from the documents
-    const items = [];
-    snapshot.forEach((doc) => {
-      const data = doc.data();
-      items.push({ name: data.name });
-    });
-
-    res.json(items);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'An error occurred while fetching item names'});
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the item" });
   }
 });
 
--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 // Initialize dotenv
 dotenv.config();
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "",
   }),
 });
 
+// Export the Firestore database
 export const db = admin.firestore();
-
