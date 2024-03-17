7e660ff1aaf6eac2c5c4716a64f542aece39f4eb
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-03-17 14:19:33 -0700

    Add CORS, change to port 3030 for testing locally with frontend
    Both servers can't run at the same time if both are using 3030. Cors policy violations also won't allow fetch calls.

--- app.mjs
+++ app.mjs
@@ -1,20 +1,25 @@
 // app.mjs
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 // Import the routers
 import productsRouter from "./routes/productsRouter.mjs";
 
 // Create a new Express application
 const app = express();
-const port = 3000;
+const port = 3030;
 
 // Use the body parser middleware to parse the body of incoming requests
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 // Declare the routes
 app.use("/", productsRouter);
-
 
 // Start the server
 app.listen(port, () => console.log(`Server has started on port: ${port}`));
