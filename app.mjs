dd4c7af08bf9fff6f9ab61174c9228023ca0d580
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-03-23 16:17:32 -0400

    feat: added swagger ui for docs

--- app.mjs
+++ app.mjs
@@ -2,12 +2,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
+
 // Import the routers
 import productsRouter from "./routes/productsRouter.mjs";
 
 // Create a new Express application
 const app = express();
 const port = 3030;
+
+// Swagger UI setup. This provide API documentation at "/api-docs" endpoint. 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Use the body parser middleware to parse the body of incoming requests
 app.use(express.json());
